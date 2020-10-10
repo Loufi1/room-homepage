de7c65dee1577e453a63373923e610fa1d3078b6
Louis-Frédéric Fortier <loufi@mbpdelofrederic.home>
2020-10-10 15:00:44 +0200

    Add fluid responsive design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8ac521b580fb54951459";
+/******/ 	var hotCurrentHash = "3edd0e2a717b28f4dfc9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
